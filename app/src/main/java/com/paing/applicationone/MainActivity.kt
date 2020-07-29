4b0059fa7ce38007b79a18445934e460b36b2d0c
Thet Paing Htwe <thatpinghtue728@gmail.com>
2020-07-29 14:58:37 +0630

    assignment_layout

---
+++ app/src/main/java/com/paing/applicationone/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.paing.applicationone
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.assingment_layout)
+    }
+}
